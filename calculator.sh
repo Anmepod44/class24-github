d5fa1c36679e447cd52ed4c60124cff2a0fb47b9
obodoekwe <obodoekwesam@gmail.com>
2025-08-12 13:40:20 +0000

    Assignment commit updated operation 4

--- calculator.sh
+++ calculator.sh
@@ -12,10 +12,10 @@ echo "Select your operation"
 echo "1. Addition (+)"
 echo "2. Subtraction : subtract items (-)"
 echo "3. Multiplication (*)"
-echo "4. Division (//) "
+echo "4. Division (/) "
 
 # Read the selected user choice
-read -p "Select operator between 1 and 2 [1-2]: " choice
+read -p "Select operator between 1 and 4 [1-4]: " choice
 
 # Perform arithmetic operation
 case "$choice" in
@@ -32,9 +32,13 @@ case "$choice" in
     echo "Result: $num1 * $num2 = $result"
     ;;
 
-  4) result=$(echo "$num1 // $num2" | bc -l)
-    echo "Result: $num1 // $num2 = $result"
-    ;;  
+  4) if [ "$num2" == "0" ]; then
+      echo "Error: Division by zero is not allowed."
+    else
+      result=$(echo "scale=2; $num1 / $num2" | bc)
+      echo "Result: $num1 / $num2 = $result"
+    fi
+    ;;
   *)
     echo "Invalid choice"
     exit 1
