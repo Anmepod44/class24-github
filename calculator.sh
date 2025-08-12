28f20178cb5a2d21f3e4927e30d63e435976b406
BodjeBodje <bouriahdiallo93@gmail.com>
2025-08-12 01:32:06 +0000

    division calculotor

--- calculator.sh
+++ calculator.sh
@@ -12,10 +12,11 @@ echo "Select your operation"
 echo "1. Addition (+)"
 echo "2. Subtraction : subtract items (-)"
 echo "3. Multiplication (*)"
-echo "4. Modulus (%)"
+echo "4. Division (/)"
+echo "5. Modulus (%)"
 
 # Read the selected user choice
-read -p "Select operator between 1 and 4 [1-4]: " choice
+read -p "Select operator between 1 and 5 [1-5]: " choice
 
 # Perform arithmetic operation
 case "$choice" in
@@ -35,6 +36,9 @@ case "$choice" in
     result=$(echo "$num1 % $num2" | bc -l)
     echo "Result: $num1 % $num2 = $result"
     ;;
+  5)result=$(echo "$num1 / $num2" | bc -l)
+    echo "Result: $num1 / $num2 = $result"
+    ;;  
   *)
     echo "Invalid choice"
     exit 1
