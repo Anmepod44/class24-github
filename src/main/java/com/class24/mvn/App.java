57158a487cb45c7c891713ec73f1a2048b45c3f4
marvin <nyarangamarvin@gmail.com>
2025-08-23 16:35:58 +0000

    class24-maven-build initial files

---
+++ src/main/java/com/class24/mvn/App.java
@@ -0,0 +1,11 @@
+package com.class24.mvn;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+    
+        System.out.println("Hello World Class 24 Maven!");
+    }
+}
